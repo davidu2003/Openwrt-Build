bb0c663e8c39ce00d6b5a2ef22bf5c57d21f0e94
davidu2003 <127939717+davidu2003@users.noreply.github.com>
2024-12-23 22:56:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,9 @@
 git clone --depth=1 https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
-#git clone --depth=1 https://github.com/QiuSimons/openwrt-mos.git package/mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 设置默认 IP 地址为 10.10.10.1
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
